fc1e585cfd13593995db342952d86dab74329fbb
Paul Müller <paul@Air-von-Paul.Speedport_W723_V_Typ_A_1_01_018>
2021-10-02 20:07:08 +0200

    added implementation for ADC and reading of moisture-sensor

--- arduino/main.c
+++ arduino/main.c
@@ -52,17 +52,67 @@ void putDec(int16_t x) {
     }
 }
 
+
+
+void ADC_init(void){
+    //Einschalten des Enable bits für den ADC
+    ADCSRA |= (1<<ADEN) | 7;
+    //Voltage Reference Selection
+    //AVCC with external capacitor at AREF pin
+    ADMUX |= (1<<REFS0);
+    ADMUX |= (1<<REFS1);
+    ADMUX |= (0<<MUX0);
+    ADMUX |= (1<<MUX1);
+    ADMUX |= (0<<MUX2);
+    ADMUX |= (0<<MUX3);
+
+    ACSR = (0<<ACD);
+
+    
+}
+
+
+
+void ADC_read(void){
+
+    int16_t data;
+    
+    //put ADC in conversion mode
+    ADCSRA |=  (1<<ADSC); 
+
+    //Check for if ADC is in conversion mode
+    while(ADCSRA & (1<<ADSC)){}
+                                 
+
+    data = ADC;
+    putDec(data);
+    put_c('\n');
+
+}
+
+
+
+void SENSOR_init(void){
+    DDRD = (0<<DDD2);
+}
+
 // simple test program to print Sensor data
 //SETUP:
 int __attribute__((OS_main)) main(void) {
     USART_init();   // initialize serial
+    SENSOR_init();
+    ADC_init();
     put_c('R');     // signal reset
     put_c('\n');
+
+
 
 //LOOP:
     
     while (1) {
-        
+        ADC_read();
+        put_c('\n');
+        _delay_ms(1000);
         
     }
 }
